e0296c2b140c8dd35fffb10763e7d1b84d31d7bd
corn-cloud <comowaiye@gmail.com>
2020-04-02 14:08:44 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,20 @@
+// const ReactSixteenAdapter = require("enzyme-adapter-react-16");
+import React from 'react'
 // your Bomb code here!
+class Bomb extends React.Component{
+    constructor(props){
+        super()
+
+        this.state={
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render(){
+        const sentence = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+        return (
+        <div>{sentence}</div>
+        )
+    }
+}
+export default Bomb
